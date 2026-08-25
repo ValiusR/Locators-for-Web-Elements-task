d7cb973f489cf106d8c1cc160f04e5b4a9221be8
ValiusR <valius.ramanauskas@gmail.com>
2026-08-25 22:54:16 +0300

    Removed unecesary dismissonetrust

--- Locators for Web Elements/User Behaviour/Hooks.cs
+++ Locators for Web Elements/User Behaviour/Hooks.cs
@@ -20,8 +20,7 @@ public sealed class TestHooks
     [BeforeScenario]
     public void BeforeScenario(ScenarioContext scenarioContext)
     {
-        _context.Logger.Information("Scenario starting: {ScenarioTitle}", scenarioContext.ScenarioInfo.Title);
-        ConsentHelper.DismissOneTrustCookies(_context.Driver);
+        _context.Logger.Information("Scenario starting: {ScenarioTitle}", scenarioContext.ScenarioInfo.Name);
     }
 
     [AfterScenario]
