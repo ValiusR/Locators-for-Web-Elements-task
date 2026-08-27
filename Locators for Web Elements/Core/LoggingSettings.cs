7fd4506de557f22582f83648428b3ec7e4a3675b
ValiusR <valius.ramanauskas@gmail.com>
2026-08-27 09:54:34 +0300

    .

--- Locators for Web Elements/Core/LoggingSettings.cs
+++ Locators for Web Elements/Core/LoggingSettings.cs
@@ -3,7 +3,7 @@ namespace Locators_for_Web_Elements.Core;
 public class LoggingSettings
 {
     public string MinLevel { get; set; } = "Information";
-    public string FilePath { get; set; } = "Logs/epam-taf-.log";
+    public string FilePath { get; set; } = "Logs/epam-taf-{Date}.log";
     public bool ConsoleOutput { get; set; } = true;
     public int RetainedFileCountLimit { get; set; } = 7;
 }
